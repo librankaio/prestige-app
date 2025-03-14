efe060b1413c5cdb096b6da64fd1ce17dd3ab12a
Muhammad Rafiq Zayana <95515543+librankaio@users.noreply.github.com>
2025-03-14 21:09:36 +0700

    testing 14032025 v2

--- app/Http/Controllers/ControllerMitemV2.php
+++ app/Http/Controllers/ControllerMitemV2.php
@@ -33,7 +33,7 @@ class ControllerMitemV2 extends Controller
             
             // Create an instance of the image
             $img = Image::make($image);
-            
+            dd($img);
             // Compress the image (set the quality to 70)
             $img->encode('jpg', 50); // 70 is the quality (0 to 100)
 
