c67f5480c56a94512966b39da5a161457adf7c7c
Muhammad Rafiq Zayana <95515543+librankaio@users.noreply.github.com>
2025-03-14 21:35:21 +0700

    testing 14032025 v12

--- app/Http/Controllers/ControllerMitemV2.php
+++ app/Http/Controllers/ControllerMitemV2.php
@@ -41,7 +41,7 @@ class ControllerMitemV2 extends Controller
             Storage::put('images/'.$originalName, $img);
             
             $file_link = 'prestige.swiapps.com/storage/images/'.$originalName;
-            // dd($file_link);
+            dd($basenum[0]);
 
             MitemV2::create([
                 'basenum' => (string) $basenum,
